87103cddcdbbeaa2d6554b45d22a525f461e0378
Artem Oleksandrenko <artem2008.zp@gmail.com>
2024-08-16 19:40:18 +0300

    Update Js, HTML and CSS code

--- index.js
+++ index.js
@@ -41,25 +41,57 @@
 //     });
 // }
 
-const h2Block = document.querySelector('#block > h2');
-h2Block.style.backgroundColor = 'blue';
-h2Block.style.color = 'white';
+// const h2Block = document.querySelector('#block > h2');
+// h2Block.style.backgroundColor = 'blue';
+// h2Block.style.color = 'white';
 
-const buttonsBlock = document.querySelectorAll('#block > .button');
-buttonsBlock.forEach(element => {
-    element.style.backgroundColor = 'yellow';
+// const buttonsBlock = document.querySelectorAll('#block > .button');
+// buttonsBlock.forEach(element => {
+//     element.style.backgroundColor = 'yellow';
+// });
+
+// for (const buttonBlock of buttonsBlock) {
+//     buttonBlock.style.fontSize = '16px';
+// }
+
+// const lisBlock = document.querySelectorAll('#block > ol > li');
+// for (const element of lisBlock) {
+//     element.addEventListener('click', () => {
+//         element.textContent = 'wow';
+//     });
+// }
+
+// document.querySelector('#block > ol > li:first-child').style.backgroundColor = 'pink';
+// document.querySelector('#block > ol > li:last-child').style.backgroundColor = 'pink';
+
+// const buttons = document.querySelectorAll('button');
+// for (const button of buttons) {
+//     button.className = 'button';
+//     button.addEventListener('mouseenter', () => {
+//         button.classList.add('active');
+//     })
+//     button.addEventListener('mouseout', () => {
+//         button.classList.remove('active');
+//     })
+//     button.addEventListener('click', () => {
+//         button.classList.toggle('shadow');
+//     })
+// }
+
+// const block1 = document.getElementById('block');
+// block1.addEventListener('click', () => {
+//     block1.classList.toggle('big-text');
+// })
+
+// const button = document.querySelector('#block > button');
+// const picture = document.querySelector('#block > img');
+// button.addEventListener('click', () => {
+//     button.textContent = button.textContent === 'hide picture' ? 'show picture' : 'hide picture';
+//     picture.classList.toggle('hide');
+// })
+
+const buttonTheme = document.querySelector('button:first-of-type');
+buttonTheme.addEventListener('click', () => {
+    buttonTheme.textContent = buttonTheme.textContent === 'light' ? 'dark' : 'light';
+    document.body.classList.toggle('dark');
 });
-
-for (const buttonBlock of buttonsBlock) {
-    buttonBlock.style.fontSize = '16px';
-}
-
-const lisBlock = document.querySelectorAll('#block > ol > li');
-for (const element of lisBlock) {
-    element.addEventListener('click', () => {
-        element.textContent = 'wow';
-    });
-}
-
-document.querySelector('#block > ol > li:first-child').style.backgroundColor = 'pink';
-document.querySelector('#block > ol > li:last-child').style.backgroundColor = 'pink';
